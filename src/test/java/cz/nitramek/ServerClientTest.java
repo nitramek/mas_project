a33694cf82d596456a100aea6fce0706dca1dd84
Martin Vaněk <vanekm@vrk.cz>
2017-09-26 09:23:02 +0200

    Now test for 3 Hello messages and the test passes if at least two of them go through

--- src/test/java/cz/nitramek/ServerClientTest.java
+++ src/test/java/cz/nitramek/ServerClientTest.java
@@ -9,14 +9,16 @@ import cz.nitramek.messaging.network.UDPSender;
 import cz.nitramek.utils.NetworkUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Slf4j
 class ServerClientTest {
@@ -47,18 +49,31 @@ class ServerClientTest {
     void helloTest() throws Exception {
         Lock lock = new ReentrantLock();
         Condition condition = lock.newCondition();
+        int counters[] = {0, 0};
         receiver.addMessageListener(message -> {
+            counters[0]++;
             lock.lock();
-            Assertions.assertEquals(message, "Hello", "Message was incorrect");
-            condition.signal();
+            if (message.equals("Hello")) {
+                counters[1]++;
+            }
+            if (counters[1] > 2) {
+                condition.signal();
+            }
+            if (counters[0] == 3) {
+                condition.signal();
+
+            }
             lock.unlock();
 
         });
 
         sender.sendPacket(new Packet("Hello".getBytes(StandardCharsets.UTF_8), receiver.getAddress()));
+        sender.sendPacket(new Packet("Hello".getBytes(StandardCharsets.UTF_8), receiver.getAddress()));
+        sender.sendPacket(new Packet("Hello".getBytes(StandardCharsets.UTF_8), receiver.getAddress()));
         lock.lock();
-        condition.await();
+        condition.await(1, TimeUnit.SECONDS);
         lock.unlock();
+        assertTrue(counters[1] > 2, String.format("Messages count %s, hellos count %s", counters[0], counters[1]));
 
     }
 }
